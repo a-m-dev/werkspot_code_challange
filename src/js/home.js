31c380b476b957f2a677116a76b8bfa77982f36a
a-m-dev <a.mirzaei.dev@gmail.com>
2020-08-16 02:26:18 +0430

    basic post list status handeling

--- src/js/home.js
+++ src/js/home.js
@@ -20,5 +20,43 @@
     main();
   }
 
-  async function main() {}
+  async function main() {
+    errorBox.classList.add("visible-off");
+    postListContainer.classList.add("visible-off");
+    loadingIndicator.classList.add("visible-on--flex");
+
+    try {
+    } catch (err) {
+      console.error(err);
+      toggleVisibility({ element: errorBox, isFlex: true });
+    } finally {
+      toggleVisibility({ element: loadingIndicator, isFlex: true });
+    }
+  }
+
+  /** Utils */
+  function toggleVisibility({ element, isFlex = false }) {
+    switch (true) {
+      case element.classList.contains("visible-on"): {
+        element.classList.remove("visible-on");
+        element.classList.add("visible-off");
+        break;
+      }
+      case element.classList.contains("visible-on--flex"): {
+        element.classList.remove("visible-on--flex");
+        element.classList.add("visible-off");
+        break;
+      }
+
+      case element.classList.contains("visible-off"): {
+        element.classList.remove("visible-off");
+        isFlex
+          ? element.classList.add("visible-on--flex")
+          : element.classList.add("visible-on");
+        break;
+      }
+      default:
+        break;
+    }
+  }
 });
